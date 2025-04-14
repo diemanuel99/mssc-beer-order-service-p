9ce82bcd5526fccb249fe61c1992b5fb4f8c3d96
Diego Emanuel Duarte Cardozo <dduarte@roshka.com>
2025-04-14 17:14:37 -0300

    gradle plattform

--- src/main/java/guru/sfg/beer/order/service/web/mappers/BeerOrderLineMapper.java
+++ src/main/java/guru/sfg/beer/order/service/web/mappers/BeerOrderLineMapper.java
@@ -2,11 +2,19 @@ package guru.sfg.beer.order.service.web.mappers;
 
 import guru.sfg.beer.order.service.domain.BeerOrderLine;
 import guru.sfg.beer.order.service.web.model.BeerOrderLineDto;
+import org.mapstruct.DecoratedWith;
 import org.mapstruct.Mapper;
+import org.mapstruct.Mapping;
 
 @Mapper(uses = {DateMapper.class})
+@DecoratedWith(BeerOrderLineMapperDecorator.class)
 public interface BeerOrderLineMapper {
+    @Mapping(target = "price", ignore = true)
+    @Mapping(target = "beerStyle", ignore = true)
+    @Mapping(target = "beerName", ignore = true)
     BeerOrderLineDto beerOrderLineToDto(BeerOrderLine line);
 
+    @Mapping(target = "quantityAllocated", ignore = true)
+    @Mapping(target = "beerOrder", ignore = true)
     BeerOrderLine dtoToBeerOrderLine(BeerOrderLineDto dto);
 }
